bd81cf7983eee1fa8918f2da9f7d7c252935de9f
guoshibo <1137603852@qq.com>
2019-04-16 19:12:15 +0800

    42

--- 42-password-visual/main.js
+++ 42-password-visual/main.js
@@ -1,3 +1,12 @@
-$(function(){
-  console.log('hello world!');
-});
+/* exported PasswordTextbox */
+var PasswordTextbox = function(config) {
+  var $pwd = $('<input type="password">'),
+      $eye = $('<div></div>'),
+      $div = $('<div class="wd-password">密码：</div>');
+  $div.append($pwd);
+  $div.append($eye);
+  $(config.container).append($div);
+  this.getPwd = function(){
+    return $pwd.val();
+  };
+};

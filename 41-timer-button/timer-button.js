f7969ce0529646c40147d9d933c680b631f9c254
guoshibo <1137603852@qq.com>
2019-04-16 17:07:46 +0800

    41

--- 41-timer-button/timer-button.js
+++ 41-timer-button/timer-button.js
@@ -1,47 +1,37 @@
-/* exported $timerBtn */
-var $timerBtn=(function(){
-  var cfg={
-    container:'.main',
-    title:'确定',
-    time:9,
-    enable:false,
-    clickHandler:null
-  };
+/* exported TimerButton */
+var TimerButton = function(config) {
+  var $btn, 
+      i = config.duration,
+      html = '<input class="timer-button" type="button" value="">',
+      enabled = config.enabled,
+      timer;
+  $(config.container).html(html);
+  $btn = $(config.container + '>input.timer-button');
+  ($btn.reset = function(enabledBtn) {
+    enabled = (enabledBtn === undefined) ? enabled : enabledBtn;
+    $btn.val(btnText());
+    if(enabled) return;
+    $btn.attr('disabled', 'disabled');
+    timer = window.setInterval(function() {
+      i--;
+      $btn.val(btnText());
+      if(i === 0) enableBtn();
+    }, 1000);                        
+  })();
+  
+  function enableBtn() {
+    enabled = true;
+    i = config.duration;
+    window.clearInterval(timer);
+    $btn.val(btnText());
+    $btn.removeAttr('disabled');
+  }
+  function btnText() {
+    return enabled ? config.text : config.text + ' (' + i + ' s)';
+  }
+  $btn.click(function() {
+    $btn.trigger('timer-button-click');
+  });
+  return $btn;
 
-  var n = cfg.time;
-  var init = function(conf){
-    $.extend(cfg,conf);
-    var $contaniner = $(cfg.container);
-    /*
-    var $timerBtn = $('<input type="button" value="同意(6s)" disabled>');
-    $timerBtn.appendTo($container);
-    $container.append($timerBtn);
-    */
-    var DOM = '<input class="timer-button" type="button" value="'+cfg.title+'('+n+'s)" disabled >';
-    $contaniner.html(DOM);
-    var $btnAgree = $contaniner.find('.timer-button');
-    //css内部样式
-    /*$btnAgree.css({
-    'font-size':'1.2em',
-    'height':'60px',
-    'width':'200px'
-    });
-    */
-    // $btnAgree.attr('disabled','disabled');
-    var timer = window.setInterval(function(){
-      n--;
-      if(n===0){
-        window.clearInterval(timer);
-        $btnAgree.removeAttr('disabled');
-        $btnAgree.val(cfg.title);
-      }else{
-        $btnAgree.val(cfg.title+'('+n+'s)');
-      } 
-    },1000);
-
-    $btnAgree.click(function(){
-      cfg.clickHandler();
-    });
-  };
-  return {init:init,remove:remove};
-}());
+};
